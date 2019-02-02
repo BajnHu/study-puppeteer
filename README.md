ab68503e4c7eb94dd20852769f75554672f02efe
hubaojun <hubaojun@021.com>
2019-02-02 11:38:32 +0800

    数据库结构调整，getJokeData 更改为分屏返回

---
+++ README.md
@@ -0,0 +1,3 @@
+#make-joke-data
+###describe
+    - 
--- app.js
+++ app.js
@@ -6,28 +6,28 @@ const JokeModel = require('./src/db/jokeSchema')
 
 
 mongoose.connect('mongodb://localhost:27017/jokeCang', {
-    useNewUrlParser: true
+  useNewUrlParser: true
 }, function (err, db) {
-    if (err) {
-        console.log('error:' + err)
-    } else {
-        console.log('success')
-    }
+  if (err) {
+    console.log('error:' + err)
+  } else {
+    console.log('success')
+  }
 })
 
 
 // 设置 响应头
 app.all('*', function (req, res, next) {
-    // 解决跨域
-    res.header("Access-Control-Allow-Origin", "*");
-    //响应头 请按照自己需求添加。
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    //支持的http 动作
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1');
-    // 编码
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
+  // 解决跨域
+  res.header("Access-Control-Allow-Origin", "*");
+  //响应头 请按照自己需求添加。
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  //支持的http 动作
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1');
+  // 编码
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
 });
 
 
@@ -35,57 +35,48 @@ console.log(`server start in port: ${port}`)
 
 
 app.get('/api/getJokeData', (req, res) => {
-    let data
-    let code
-    // let _callback = req.query.jsonpCallback;
-    // 有元素索引  并且 不为空
-    let { startkey = '' } = req.query;
+  let data
+  let code
+  // 有元素索引  并且 不为空
+  let {startkey = '', jsonpCallback} = req.query;
 
-    // 数据库 根据index 查询
-    JokeModel.findPage({ startkey }, function (err, jokes) {
-        if (err) {
-            data = null
-            code = 1
-            res.status(500)
-            console.log('err:' + err)
-        } else {
-            if (jokes) {
-                data = jokes
-                code = 0
-            } else {
-                data = null
-                code = 1
-            }
-            res.status(200)
-        }
+  // 数据库 根据index 查询
+  JokeModel.findPage({startkey}, function (err, jokes) {
+    if (err) {
+      data = null
+      code = 1
+      res.status(500)
+      console.log('err:' + err)
+    } else {
+      if (jokes) {
+        data = jokes
+        code = 0
+      } else {
+        data = null
+        code = 1
+      }
+      res.status(200)
+    }
 
-        let resData = data.map((item, idx) => {
-            let { title, content, id, type, tags } = item
-            return {
-                title, content, id, type, tags
-            }
-        })
-
-        let endkey = data[data.length - 1]._id;
-        // if (_callback) {
-        // res.type('text/javascript');
-        // res.send(_callback + '(' + JSON.stringify({
-        //     data,
-        //     date: Date.now(),
-        //     code
-        // }) + ')');
-        res.type('text/json');
-        res.send({
-            data: resData,
-            date: Date.now(),
-            endkey,
-            code
-        });
-        // }
+    let resData = data.map((item, idx) => {
+      let {title, content, id, type, tags} = item
+      return {
+        title, content, id, type, tags
+      }
     })
+
+    let endkey = data[data.length - 1]._id;
+    // 有callback
+
+    res.type('text/javascript');
+    res.send(jsonpCallback + '(' + JSON.stringify({
+      data: resData,
+      date: Date.now(),
+      endkey,
+      code
+    }) + ')');
+  })
 })
-
-
 
 
 app.listen(port)
